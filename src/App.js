cd75b5d5b082d89f3f3125753f5fbdf4ac400190
rafif113 <rafifyusuf@gmail.com>
2020-10-13 17:57:01 +0700

    Upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First Heading</h1>
+      <p>My first paragraph.</p>
+    </div>
+  );
+}
+
+export default App;
